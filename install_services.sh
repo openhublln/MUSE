9eb1bdb23c746b42cae6f3e3b5e95ab05298502a
Alexandre Vogel <alexandre.vogel@uclouvain.be>
2025-10-14 17:42:39 +0200

    add script to install services

---
+++ install_services.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# lidar
+cp ./lidar/lidar@.service /etc/systemd/system/lidar@.service
+
+# camera
+cp ./camera/camera@.service /etc/systemd/system/camera@.service
+
+# obd
+cp ./obd/obd@.service /etc/systemd/system/obd@.service
+
+# communication
+cp ./communication/gpsd_exporter/gpsd_exporter.service /etc/systemd/system/gpsd_exporter.service
+cp ./communication/iperf/iperf_logger.service /etc/systemd/system/iperf_logger.service
+cp ./communication/rm500u-manager/rm500u_logger.service /etc/systemd/system/rm500u_logger.service
+cp ./communication/traceroute/traceroute_loggerV4.service /etc/systemd/system/traceroute_loggerV4.service
+cp ./communication/traceroute/traceroute_loggerV6.service /etc/systemd/system/traceroute_loggerV6.service
