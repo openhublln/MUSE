3236d8d795020fc9ec49bb135052754de338500b
Alexandre Vogel <alexandre.vogel@uclouvain.be>
2025-10-14 17:27:23 +0200

    remove obsolete camera  and cleanup

--- stop_muse.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# setting the sudo password
-SUDO_PASSWORD="openhub"
-
-echo $SUDO_PASSWORD | sudo -S systemctl stop muse_camera.path
-echo $SUDO_PASSWORD | sudo -S systemctl stop muse.service
