ad7c0aa410122d58e25834a833ad39cc02a7c306
Alexandre Vogel <alexandre.vogel@uclouvain.be>
2025-10-15 15:46:14 +0200

    Add start and stop scripts

---
+++ start.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+DATA_DIR="/home/openhub/data"
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+OUTPUT_DIR=$DATA_DIR/$TIMESTAMP
+
+BT_ADDRESS="66:1E:32:30:33:38"
+
+# create new directory with timestamp
+mkdir -p $OUTPUT_DIR
+
+# launch services
+
+# lidar
+mkdir -p $OUTPUT_DIR/lidar
+systemctl start lidar@$OUTPUT_DIR/lidar
+
+# camera
+mkdir -p $OUTPUT_DIR/camera
+systemctl start camera@$OUTPUT_DIR/camera
+
+# obd
+## Bluetooth pairing
+rfcomm bind /dev/rfcomm1  $BT_ADDRESS
+
+mkdir -p $OUTPUT_DIR/obd
+systemctl start obd@$OUTPUT_DIR/obd
+
+# communication
+## gps
+#systemctl start gpsd_exporter
+## modem
+systemctl start rm500u_logger
+## performance measurement
+systemctl start iperf_logger
+systemctl start traceroute_loggerV4
+systemctl start traceroute_loggerV6
---
+++ stop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+systemctl stop lidar@*
+systemctl stop camera@*
+systemctl stop obd@*
+systemctl stop gpsd_exporter
+systemctl stop rm500u_logger
+systemctl stop iperf_logger
+systemctl stop traceroute_loggerV4
+systemctl stop traceroute_loggerV6
