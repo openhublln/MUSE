66d6325d71bfdf6a4525f7975ec8cf925e6e0ff9
LucasElRag <139466069+LucasElRag@users.noreply.github.com>
2025-12-08 15:43:58 +0100

    added radar service stop (commented out)

--- stop.sh
+++ stop.sh
@@ -3,6 +3,7 @@
 systemctl stop lidar@*
 systemctl stop camera@*
 systemctl stop obd@*
+# systemctl stop radar@*
 systemctl stop gpsd_exporter
 systemctl stop rm500u_logger
 systemctl stop iperf_logger
